a24960a6215dbccedb770fc5fa969d68c07d07dd
Lucas Caton <lucascaton@gmail.com>
2014-06-01 13:46:56 +1000

    Patients - Export to CSV feature

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -5,6 +5,20 @@ class PatientsController < ApplicationController
     @patients = @patients.order('patients.lastname')
   end
 
+  def csv
+    file = "#{Rails.root}/tmp/patients_#{Time.now.to_i}.csv"
+
+    CSV.open(file, 'w') do |csv|
+      csv << %w(id kidzcan_number hospital_ref_number firstname lastname sex date_of_birth date_of_death kidzcan_registration mobile_number status point_of_contact diagnosis created_at updated_at)
+
+      @patients.each do |patient|
+        csv << patient.slice(:id, :kidzcan_number, :hospital_ref_number, :firstname, :lastname, :sex, :date_of_birth, :date_of_death, :kidzcan_registration, :mobile_number, :status, :point_of_contact, :diagnosis, :created_at, :updated_at).values
+      end
+    end
+
+    send_file(file)
+  end
+
   def edit
   end
 
@@ -31,5 +45,4 @@ class PatientsController < ApplicationController
   def patient_params
     params.require(:patient).permit(:firstname)
   end
-
 end
